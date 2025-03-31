90e90aaa57b4848ef0938529eebf7abbccf5c43a
pdridh <91915381+pdridh@users.noreply.github.com>
2025-03-31 16:44:54 +0545

    Create auth handler for login

--- backend/auth/handler.go
+++ backend/auth/handler.go
@@ -67,3 +67,49 @@ func (h *Handler) Register() http.HandlerFunc {
 		api.WriteJSON(w, r, http.StatusCreated, u)
 	}
 }
+
+func (h *Handler) Login() http.HandlerFunc {
+	type LoginPayload struct {
+		Email    string `json:"email" validate:"required,email"`
+		Password string `json:"password" validate:"required,min=8,max=80"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Read body into the payload
+		var p LoginPayload
+
+		if err := api.ParseJSON(r, &p); err != nil {
+			api.WriteError(w, r, http.StatusBadRequest, "Bad json request", nil)
+			return
+		}
+
+		var validationErrs []error
+		if err := h.Service.validate.Struct(p); err != nil {
+			for _, e := range err.(validator.ValidationErrors) {
+				validationErrs = append(validationErrs, api.NewFieldError(e.Field(), e.Tag(), e.Value()))
+			}
+		}
+
+		if len(validationErrs) > 0 {
+			// Handle all errors
+			api.WriteError(w, r, http.StatusBadRequest, "Invalid form body", validationErrs)
+			return
+		}
+
+		t, err := h.Service.AuthenticateUser(p.Email, p.Password)
+		if err != nil {
+			switch err {
+			case ErrUnknownEmail, ErrWrongPassword:
+				api.WriteError(w, r, http.StatusUnauthorized, "Invalid credentials", nil)
+				return
+			default:
+				api.WriteInternalError(w, r)
+				return
+			}
+		}
+
+		// Passed everything meaning the authentication was succesful, lets give the user a token
+		SetJWTCookie(w, t)
+		api.WriteJSON(w, r, http.StatusOK, "Login succesfull!")
+	}
+}
