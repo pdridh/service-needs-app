d3b49d48ae3f414a5142465a6357f223c20a2dd3
pdridh <91915381+pdridh@users.noreply.github.com>
2025-03-31 20:44:01 +0545

    Define simple type for Provider (incomplete)

---
+++ backend/provider/types.go
@@ -0,0 +1,14 @@
+package provider
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Provider struct {
+	ID          primitive.ObjectID `json:"id" bson:"_id"`
+	UserID      primitive.ObjectID `json:"-" bson:"user_id"`
+	Name        string             `json:"name" bson:"name"`
+	Category    string             `json:"category" bson:"category"`
+	Location    string             `json:"location" bson:"location"` // TODO change this to somehting better like coords or something idk
+	Description string             `json:"description" bson:"description"`
+	CreatedAt   primitive.DateTime `json:"created_at" bson:"created_at"`
+	// TODO add other information like available time, documents, profile stuff etc,etc...
+}
